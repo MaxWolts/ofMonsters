0734fdcf0f52b9a8360e9672596deb56e4812d12
MaxWolts <max.wolts@gmail.com>
2025-11-14 18:14:41 -0300

    :wrench: Config prubes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
 import tailwind from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel(),
+  output: 'static',
   vite: {
     plugins: [tailwind()],
   },
